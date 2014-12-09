4c631edfa8dc792916627a0bcffc32714159de07
yifeng <493788939lizhaogai@163.com>
2014-12-09 10:29:46 +0800

    c

--- bin/zbStartUp.sh
+++ bin/zbStartUp.sh
@@ -2,7 +2,7 @@
 zbStartUpLog=/usr/local/silo/agent/zbStartUp.log
 zbServerLog=/usr/local/silo/agent/zbServer.log
 
-/usr/local/silo/agent/bin/setuplprfcape_reva1.sh
+/usr/local/silo/agent/drivers/ollo-zigbee/bin/setuplprfcape_reva1.sh
 
 #check ttyACM0-4
 zbCdcDev=/dev/ttyACM
@@ -10,7 +10,7 @@ zbDevCdcIdx=0
 for zbDevCdcIdx in 0 1 2 3 4
 do
 	echo "getFwModel from $zbCdcDev$zbDevCdcIdx" >> $zbStartUpLog
-	modelId="$(/usr/local/silo/agent/bin/zbFwStart.linux.arm.bin $zbCdcDev$zbDevCdcIdx)"
+	modelId="$(/usr/local/silo/agent/drivers/ollo-zigbee/bin/zbFwStart.linux.arm.bin $zbCdcDev$zbDevCdcIdx)"
 
 	if [[ "$modelId" =~ "Model ID" ]]; then
 		zbDev=$zbCdcDev$zbDevCdcIdx
@@ -23,7 +23,7 @@ done
 if [[ -z "$zbDev" ]]; then
 	zbttyDev=/dev/ttyO4
         echo "getFwModel from $zbttyDev" >> $zbStartUpLog
-        modelId="$(/usr/local/silo/agent/bin/zbFwStart.linux.arm.bin $zbttyDev)"
+        modelId="$(/usr/local/silo/agent/drivers/ollo-zigbee/bin/zbFwStart.linux.arm.bin $zbttyDev)"
 
         if [[ "$modelId" =~ "Model ID" ]]; then
                 zbDev=$zbttyDev
@@ -35,11 +35,11 @@ fi
 
 if [[ $modelId =~ "ZBGW" ]] ; then
 	echo "Starting zbGateway with $zbDev" >> $zbStartUpLog
-	/usr/local/silo/agent/bin/zbGateway.linux.arm.bin $zbDev > $zbServerLog 2>&1
+	/usr/local/silo/agent/drivers/ollo-zigbee/bin/zbGateway.linux.arm.bin $zbDev > $zbServerLog 2>&1
 else
 	if [[ $modelId =~ "TI SampleBridge" ]] ; then
         	echo "Starting zllGateway with $zbDev" >> $zbStartUpLog
-        	/usr/local/silo/agent/bin/zllGateway.linux.arm.bin $zbDev > $zbServerLog 2>&1
+        	/usr/local/silo/agent/drivers/ollo-zigbee/bin/zllGateway.linux.arm.bin $zbDev > $zbServerLog 2>&1
 	else
                 echo "Unknown zb device" >> $zbStartUpLog
 	fi
